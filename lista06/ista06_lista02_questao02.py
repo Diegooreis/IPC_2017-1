90c055a80acfe0f608d1be7ce1089a9148316728
Diegooreis <dieguitoreis7@gmail.com>
2017-05-02 01:10:36 -0400

    Update lista06_lista02_questao02.py

---
+++ lista06/ista06_lista02_questao02.py
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# André Luis Laborda Neves              1515070006 
+# João Vitor de Cordeiro B. Gonçalves   1515140036
+# Gabriel Nascimento de Oliveira        1715310052
+# Diego Reis figueira                   1515070169
+# Diogo Roberto Duarte da Costa         1715310056
+
+#Escreva um algoritmo em PORTUGOL que armazene em um vetor todos os números
+#inteiros do intervalo fechado de 1 a 100. Após isso, o algoritmo deve imprimir todos
+#os valores armazenados.
+
+
+vetor = []
+cont = 0
+number = 1
+while (cont < 100):
+    if (number != 0):
+        number = int(input("Digite um número: "))
+        vetor.append(number)
+        cont += 1
+print (vetor)
