2381250dee6f6136d3f37349910d4d6d6e7393e1
Diegooreis <dieguitoreis7@gmail.com>
2017-05-02 01:08:18 -0400

    Update lista06_lista01_questao13.py

--- lista06/lista06_lista01_questao13.py
+++ lista06/lista06_lista01_questao13.py
@@ -10,8 +10,8 @@
 
 
 # Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista. Após isto, calcule a média anual das 
-temperaturas e mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram
-(mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
+#temperaturas e mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram
+#(mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
 
 temperature = []
 months = ['Janeiro','Fevereiro','Março','Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
