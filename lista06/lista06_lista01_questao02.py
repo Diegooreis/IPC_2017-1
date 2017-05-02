bf73c90e572fb50f799c88f16c896845ab9a773e
Diegooreis <dieguitoreis7@gmail.com>
2017-05-02 00:09:13 -0400

    lista06_lista01_questao02.py
    Update lista06_lista01_questao02.py

--- lista06/lista06_lista01_questao02.py
+++ lista06/lista06_lista01_questao02.py
@@ -1,13 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# André Luis Laborda Neves              1715310059
+# João Vitor de Cordeiro B. Gonçalves   1515140036
+# Gabriel Nascimento de Oliveira        1715310052
+# Diego Reis figueira                   1515070169
+# Diogo Roberto Duarte da Costa         1715310056
+
+
 #Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
 
-vet = []
+vetor = []
+cont = 0
 
-for i in range (0,10):
-
-    numbers = float(input())
-    vet.append(numbers)
-
-vet.reverse()
-print(vet)
-
+while cont < 10:
+    number = float(input("Digite um número: "))
+    vetor.append(number)
+    cont += 1
+print ("A ordem inversa é",vetor[::-1])
 
